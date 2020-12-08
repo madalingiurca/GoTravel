c0f14c32c9e6dd0ae02a878a3bfbd08cec811cab
mgiura <mgiurca.mail@gmail.com>
2020-12-09 00:28:59 +0200

    destinations fetch + edit destintion try

--- src/destination-post.js
+++ src/destination-post.js
@@ -1,50 +1,79 @@
+// noinspection JSFileReferences
 import {LitElement, html, css} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 class DestinationPost extends LitElement {
 
   constructor() {
     super();
+    this.id = '';
     this.title = '';
     this.description = '';
     this.know_more_url = '';
     this.img_url = '';
+    this.editing = false;
   }
 
   static get styles() {
-    return css`.destination {
-                    margin: 0 10% 0 10%;
-                    padding: 5px; /*un mic gap fara padding*/
-                    display: flex;
-                    flex-flow: row wrap;
-                }
-                
-                .destination div {
-                    background-color: #f0f0f0;
-                    flex: 7;
-                    padding: 5% 0 0 5%;
-                }
-                
-                .destination img {
-                    flex: 9;
-                }
-                
-                .destination h3 {
-                    color: var(--main-color);
-                    margin: 10px 0 10px 0;
-                }
-                
-                .destination div slot {
-                    margin: 10px 0 10px 0;
-                }
-                
-                .destination div {
-                    padding: 1.5%;
-                }
-                
-                a {
-                    text-decoration: none;
-                    color: darkslateblue;
-                }`;
+    return css`button {
+      position: relative;
+      left: -10px;
+      z-index: 99999;
+      border-radius: 4px;
+      background-color: var(--main-color);
+      border: none;
+      color: #FFFFFF;
+      font-size: 13px;
+      text-align: center;
+      padding: 20px;
+      transition: all 0.5s;
+      cursor: pointer;
+      margin: 5px;
+      width: 62px;
+    }
+
+      .destination {
+        margin: 0 10% 0 10%;
+        padding: 5px;
+        display: flex;
+        flex-flow: row wrap;
+      }
+
+      .destination div {
+        box-shadow: 6px 4px 7px 1px rgba(0, 0, 0, 0.82);
+        background-color: #f0f0f0;
+        flex: 7;
+        padding: 5% 0 0 5%;
+      }
+
+      .destination img {
+        flex: 9;
+      }
+
+      .destination h3 {
+        color: var(--main-color);
+        margin: 10px 0 10px 0;
+      }
+
+      .destination div p {
+        margin: 10px 0 10px 0;
+      }
+
+      .destination div {
+        padding: 1.5%;
+      }
+
+      a {
+        text-decoration: none;
+        color: darkslateblue;
+      }
+      input {
+        display: block;
+      }
+      textarea {
+        width: auto;
+        height: 70px;
+        max-width: 500px;
+      }`;
   }
 
   static get properties() {
@@ -52,27 +81,58 @@ class DestinationPost extends LitElement {
       title: {type: String},
       description: {type: String},
       know_more_url: {type: String},
-      img_url: {type: String}
+      img_url: {type: String},
+      editing: {type: Boolean}
     };
   }
 
-  attributeChangedCallback(name, oldVal, newVal) {
-    super.attributeChangedCallback(name, oldVal, newVal);
+  handleEditClick(event){
+    if(this.editing) {
+      const inputs = event.target.parentNode.childNodes[3].childNodes;
+      this.title = inputs[1].value;
+      this.description = inputs[3].value;
+      this.know_more_url = inputs[5].value;
+
+      let body = {
+        title: this.title,
+        description: this.description,
+        know_more_url: this.know_more_url,
+      }
+
+      fetch(`https://devschool-2020.firebaseio.com/madalin-giurca/destinations/${this.id}.json`, {
+        method: 'PATCH',
+        headers: {
+          'content-type' : 'application/json'
+        },
+        body: JSON.stringify(body)
+      }).then(response => console.log(response));
+
+      this.editing = !this.editing;
+    }
+    else {
+      this.editing = !this.editing;
+    }
   }
 
   render() {
-    return html`<div class="destination">
-                    <picture style="display: flex">
-                        <img src="${this.img_url}" alt="imgDestination1">
-                    </picture>
-                    <div>
-                        <h3>${this.title}</h3>
-                        <slot name="description">
-                            Destination 1 description
-                        </slot>
-                        <a href="${this.know_more_url}">> know more</a>
-                    </div>
-                </div>`;
+
+    return html`
+        <div class="destination">
+            <picture style="display: flex">
+                <img src="${this.img_url}" alt="imgDestination1">
+            </picture>
+            <div>
+                ${this.editing? 
+                        html`<input id="newTitle" value=${this.title}>
+                             <textarea id="newDescription"></textarea>
+                             <input id="newURL" value="${this.know_more_url}">`
+                        : 
+                        html`<h3>${this.title}</h3>
+                        <slot name="description"></slot>
+                        <a href="${this.know_more_url}">> know more</a>`}
+            </div>
+            <button @click="${(event) => this.handleEditClick(event)}">${this.editing?"Save":"Edit"}</button>
+        </div>`;
   }
 }
 
---
+++ src/destination-section.js
@@ -0,0 +1,90 @@
+// noinspection JSFileReferences
+import {css, html, LitElement} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
+import './article-post.js';
+
+class DestinationsSection extends LitElement {
+
+  static get styles() {
+    return css`
+      .loader {
+        margin: 40px auto;
+        border: 5px solid #f3f3f3; /* Light grey */
+        border-top: 5px solid var(--main-color); /* Blue */
+        border-bottom: 0;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        animation: spin 4s linear infinite;
+      }
+
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+
+      .animate-bottom {
+        position: relative;
+        -webkit-animation-name: animatebottom;
+        -webkit-animation-duration: 1.5s;
+        animation-name: animatebottom;
+        animation-duration: 1.5s
+      }
+
+      @-webkit-keyframes animatebottom {
+        from {
+          bottom: -120px;
+          opacity: 0
+        }
+        to {
+          bottom: 0;
+          opacity: 1
+        }
+      }
+
+      @keyframes animatebottom {
+        from {
+          bottom: -120px;
+          opacity: 0
+        }
+        to {
+          bottom: 0;
+          opacity: 1
+        }
+      }
+    `
+  }
+
+  static get properties() {
+    return {
+      destinations: {type: Array},
+    }
+  }
+
+  constructor() {
+    super();
+    fetch('https://devschool-2020.firebaseio.com/madalin-giurca/destinations.json')
+      .then(response => response.json())
+      .then(data => this.destinations = data)
+  }
+
+  render() {
+    return !this.destinations ? html`
+        <div class="loader"></div>`
+      :
+      html`${
+        Object.entries(this.destinations).map(([k, v]) => {
+          return html`
+              <destination-post class="animate-bottom" id="${k}" title="${v['title']}" img_url="${v['img_url']}">
+                  <p slot="description">${v['description']}</p>
+              </destination-post>
+        `
+        })
+      }`;
+  }
+}
+
+customElements.define('destinations-section', DestinationsSection)
